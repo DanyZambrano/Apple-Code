bbb33f778ef50321f697e3ba52bb3a5066191648
Dany Zambrano <danyzambranososa@gmail.com>
2021-09-29 20:44:17 -0500

    base code

---
+++ Shared/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  Shared
+//
+//  Created by Dany E Zambrano S on 29/09/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
