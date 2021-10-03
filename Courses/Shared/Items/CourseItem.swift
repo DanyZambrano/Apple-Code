0f6e09c6189ceca0a3b2f76c5378ed9bade99c4a
Dany Zambrano <danyzambranososa@gmail.com>
2021-10-02 21:54:06 -0500

    Finally code

--- Courses/Shared/Items/CourseItem.swift
+++ Courses/Shared/Items/CourseItem.swift
@@ -8,23 +8,30 @@
 import SwiftUI
 
 struct CourseItem: View {
+    var course: Course = courses[0]
+    #if os(iOS)
+    var cornerRadius: CGFloat = 22
+    #else
+    var cornerRadius: CGFloat = 10
+    #endif
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 4.0) {
             Spacer()
             HStack {
                 Spacer()
-                Image("Illustration 1")
+                Image(course.image)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                 Spacer()
             }
-            Text("SwiftUI for iOS 14").fontWeight(.bold).foregroundColor(Color.white)
-            Text("20 Sections").font(.footnote).foregroundColor(Color.white)
+            Text(course.title).fontWeight(.bold).foregroundColor(Color.white)
+            Text(course.subtitle).font(.footnote).foregroundColor(Color.white)
         }
         .padding(.all)
-        .background(Color.blue)
-        .cornerRadius(20.0)
-        .shadow(radius: 10)
+        .background(course.color)
+        .clipShape(RoundedRectangle(cornerRadius: cornerRadius, style: .continuous))
+        .shadow(color: course.color.opacity(0.3), radius: 20, x: 0, y: 10)
     }
 }
 
--- Courses/Shared/Navigation/ContentView.swift
+++ Courses/Shared/Navigation/ContentView.swift
@@ -8,16 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    #if os(iOS)
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    #endif
+    
     @ViewBuilder
     var body: some View {
         #if os(iOS)
-        Sidebar()
+        if horizontalSizeClass == .compact {
+            CoursesView()
+        } else {
+            CoursesView()
+        }
         #else
         Sidebar()
             .frame(minWidth: 1000, minHeight: 600)
         #endif
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
