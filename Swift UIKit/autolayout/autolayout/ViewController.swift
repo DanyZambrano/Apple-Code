33c7a9c38cd9754130c44f5435a50d67ca976638
Dany <dany.zambrano@veritasautomata.com>
2023-01-16 16:39:32 -0400

    .

---
+++ Swift UIKit/autolayout/autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autolayout
+//
+//  Created by Dany E Zambrano S on 16/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
