a6faf8fd5f0d47c0b8006f68100fa522fb2b9fdc
Dany Zambrano <danyzambranososa@gmail.com>
2023-08-21 14:39:47 -0400

    animation

---
+++ 8. Swift UI/8. Animacion/8.1. Animation con SF Symbols/Animacion/AnimacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimacionApp.swift
+//  Animacion
+//
+//  Created by Dany Zambrano on 19/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
