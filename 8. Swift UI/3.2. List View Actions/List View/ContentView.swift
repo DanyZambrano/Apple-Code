64e491c332a356425f0c628d1709d01918ba20c8
Dany Zambrano <danyzambranososa@gmail.com>
2023-08-23 12:41:40 -0400

    .

---
+++ 8. Swift UI/3.2. List View Actions/List View/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  List View
+//
+//  Created by Dany Zambrano on 18/8/23.
+//
+
+import SwiftUI
+
+var equipos = ["Cocodrilos de Caracas", "Bucaneros de La Guaira", "Broncos de Caracas", "Gaiteros del Zulia", "Guaiqueríes de Margarita", "Guaros de Lara", "Marineros de Anzoategui", "Panteras de Miranda", "Trotamundos de Carabobo"]
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            ForEach(equipos, id: \.self) { equipo in
+                ListItem(nombreImagen: equipo, nombreEquipo: equipo)
+            }
+            .listRowSeparator(.hidden)
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct ListItem: View {
+    var nombreImagen: String
+    var nombreEquipo: String
+    
+    @State private var acciones = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Image(nombreImagen)
+                .resizable()
+                .scaledToFill()
+                .frame(height: 250)
+                .cornerRadius(15)
+            
+            Text(nombreEquipo)
+                .font(.system(.title2, design: .rounded))
+        }
+        .padding(.horizontal)
+        .padding(.bottom)
+        .onTapGesture {
+            acciones.toggle()
+         }
+        .confirmationDialog("Selecciona una acción", isPresented: $acciones, titleVisibility: .visible) {
+            
+            Button("Comprar ticket") {
+                
+            }
+            Button("\(nombreImagen) es tu Favorito?") {
+                
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        
+        ContentView()
+            .preferredColorScheme(.dark)
+            .previewDisplayName("List View (Dark)")
+    }
+}
