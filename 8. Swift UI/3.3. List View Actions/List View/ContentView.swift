e52e686cc6d91226a7d5a39a7cb9c37761163a9a
Dany Zambrano <danyzambranososa@gmail.com>
2023-08-23 13:16:39 -0400

    .

---
+++ 8. Swift UI/3.3. List View Actions/List View/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  List View
+//
+//  Created by Dany Zambrano on 18/8/23.
+//
+
+import SwiftUI
+
+var equipos = ["Cocodrilos de Caracas", "Bucaneros de La Guaira", "Broncos de Caracas", "Gaiteros del Zulia", "Guaiqueríes de Margarita", "Guaros de Lara", "Marineros de Anzoategui", "Panteras de Miranda", "Trotamundos de Carabobo"]
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            ForEach(equipos, id: \.self) { equipo in
+                ListItem(nombreImagen: equipo, nombreEquipo: equipo)
+            }
+            .listRowSeparator(.hidden)
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct ListItem: View {
+    var nombreImagen: String
+    var nombreEquipo: String
+    
+    @State private var alerta = false
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Image(nombreImagen)
+                .resizable()
+                .scaledToFill()
+                .frame(height: 250)
+                .cornerRadius(15)
+            
+            Text(nombreEquipo)
+                .font(.system(.title2, design: .rounded))
+        }
+        .padding(.horizontal)
+        .padding(.bottom)
+        .onTapGesture {
+            alerta.toggle()
+         }
+        .alert("Aviso Importante", isPresented: $alerta) {
+           Button("Entiendo") {
+                
+            }
+           } message: {
+               Text("No hay Tickets disponibles!")
+           }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        
+        ContentView()
+            .preferredColorScheme(.dark)
+            .previewDisplayName("List View (Dark)")
+    }
+}
